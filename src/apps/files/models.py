dbf308c4ed43575af45802bc5a8983991c8e5a39
Jefferson472 <jefferson472@hotmail.com>
2022-10-01 14:42:35 -0300

    feat: att models, settings for rest api

--- src/apps/files/models.py
+++ src/apps/files/models.py
@@ -6,9 +6,14 @@ class File(models.Model):
     user = models.ForeignKey(
         User, related_name='files', on_delete=models.CASCADE)
     file = models.FileField(upload_to='home')
+    file_name = models.CharField(
+        default=file.name, max_length=100, unique=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'tb_files'
         ordering = ['-updated']
+
+    def __str__(self):
+        return self.file_name
--- src/setup/settings.py
+++ src/setup/settings.py
@@ -125,7 +125,17 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_URL =  '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.FileUploadParser',
+    ]
+}
