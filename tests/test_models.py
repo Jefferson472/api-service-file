27f7492771318088f9e7556b3ad3ff038a30e3ce
Jefferson472 <jefferson472@hotmail.com>
2022-10-02 20:54:34 -0300

    fix: test models

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,25 +1,37 @@
 import os
 from django.contrib.auth.models import User
-from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
 from apps.files.models import File
 
 
-class FileModelTestCase(TestCase):
+class FileModelTestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create(
             username="test_user", password="123456")
-        doc_file = os.path.join(
-            settings.BASE_DIR.parent, 'docs\Simple File Service brief.pdf')
         file_up = SimpleUploadedFile(
-            doc_file, b"file_content", content_type="application/pdf") 
-        self.file = File.objects.create(
-            user=self.user, file_name=file_up.name, file=doc_file)
+            'simple_file.pdf',
+            b"simple_content",
+            content_type="application/pdf"
+        ) 
+        File.objects.create(
+            user=self.user,
+            file_name=str(file_up.name).replace(' ', '_'),
+            file=file_up,
+        )
+        self.file = File.objects.get(pk=1)
+
+        self.client.login(username='test_user', password='123456')
+        self.client.force_authenticate(user=self.user)
+
+        res = self.client.delete('/api/file/1')
+        self.assertEqual(res.status_code, 200)
     
     def test_name(self):
-        self.assertEqual(self.file.file_name, 'Simple File Service brief.pdf')
+        self.assertEqual(self.file.file_name, 'simple_file.pdf')
+
 
     def test_path(self):
-        self.assertEqual(self.file.path, 'home')
+        self.assertEqual(self.file.path, '')
