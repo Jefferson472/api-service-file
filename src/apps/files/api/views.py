6e97a5e9dd6ca6850e1c58d5fc421dfc8a8a7a29
Jefferson472 <jefferson472@hotmail.com>
2022-10-02 08:14:20 -0300

    feat: add method to download file

--- src/apps/files/api/views.py
+++ src/apps/files/api/views.py
@@ -1,7 +1,9 @@
-import os
+import os, mimetypes
+from django.conf import settings
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, status, viewsets
 from rest_framework.response import Response
-from django.conf import settings
 
 from .serializations import UploadSerializer
 from ..models import File
@@ -12,10 +14,29 @@ class FileListView(generics.ListAPIView):
     serializer_class = UploadSerializer
 
     def list(self, request, **kwargs):
-        if kwargs.get('path'):
-            queryset = File.objects.filter(path__contains=kwargs['path'])
-            serializer = UploadSerializer(queryset, many=True)
-            return Response(serializer.data)
+        if 'path' in kwargs:
+            full_path = request.get_full_path()
+            format_list = ['.pdf', '.xml', '.txt']
+            if full_path[-4:] in format_list:
+                file_obj = get_object_or_404(
+                    File,
+                    file_name=full_path[full_path.rfind('/')+1:],
+                    path=full_path[11:full_path.rfind('/')],
+                )
+
+                media_file = os.path.join(settings.MEDIA_ROOT, str(file_obj.file))
+
+                mimetype = mimetypes.guess_type(media_file)
+                response = HttpResponse(open(media_file, 'rb'), status=200)
+                response['Content-Disposition'] = f'attachment; filename={file_obj.file_name}'
+                response['Content-Type'] = mimetype
+                return response
+            else:
+                queryset = File.objects.filter(path__contains=full_path[11:-1])
+                serializer = UploadSerializer(queryset, many=True)
+                response = serializer.data
+            return Response(response, status.HTTP_200_OK)
+        
         return super().list(request, **kwargs)
 
 class FileDetailView(generics.RetrieveAPIView):
